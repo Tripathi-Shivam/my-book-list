c28438d7cc7e4464c32dbcd08efd43989bf1a1d6
Shivam Tripathi <tripathishivam11@gmail.com>
2021-01-28 19:52:23 +0530

    feat: Add a book
    Added a funtionality for the user to add a book

--- app.js
+++ app.js
@@ -45,10 +45,25 @@ class UI {
         list.appendChild(row);
     }
 
-    static deleteBook(element){
-        if(element.classList.contains("delete")){
+    static deleteBook(element) {
+        if (element.classList.contains("delete")) {
             element.parentElement.parentElement.remove();
         }
+    }
+
+    static showAlert(message, className){
+        const div = document.createElement("div");
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector(".container");
+        const form = document.querySelector("#book-form");
+        container.insertBefore(div, form);
+
+        //Vanish in 3 seconds
+        setTimeout(() => {
+            document.querySelector(".alert").remove()
+        }, 3000);
     }
 }
 
@@ -57,4 +72,39 @@ class UI {
 // Event: Display Book (DOMContentLoaded: once the DOM is loaded)
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
+// Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+    // Prevent actuall submit
+    e.preventDefault();
 
+    // Get form values
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const isbn = document.querySelector("#isbn").value;
+
+    if (title === "" || author === "" || isbn === "") {
+        UI.showAlert("Please fill in all fields", "danger");
+    } else {
+        // Instantiate Book
+        const book = new Book(title, author, isbn);
+    
+        // Add book to UI
+        UI.addBookToList(book);
+    
+        // Show success message
+        UI.showAlert("Book Added", "success");
+
+        // Clear Fields
+        document.getElementById("book-form").reset();
+    }
+
+});
+
+//Event: Remove a Book
+//(use event propogation, where we select something above it and we target whatever is clicked inside of it)
+document.querySelector("#book-list").addEventListener("click", (e) => {
+    UI.deleteBook(e.target);
+    
+    // Show success message
+    UI.showAlert("Book Removed", "success");
+});
